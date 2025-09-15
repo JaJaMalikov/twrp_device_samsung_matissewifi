036d0520f645e6f8cbc2878eaee3a9bea86cca54
JaJaMalikov <jerome@chivet.net>
2025-09-15 22:12:42 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 ifneq ($(filter matissewifi,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/samsung/matissewifi/
+LOCAL_PATH := device/samsung/matissewifi
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
